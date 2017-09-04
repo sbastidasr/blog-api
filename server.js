944dc80e8f3638f60c33b51df4e97870c947f6b8
Sebastian Bastidas <sbastidasr@gmail.com>
2017-09-04 10:48:30 -0500

    server

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const morgan = require('morgan');
-const blogPostsRouter = require('./blogPostsRouter');
-const app = express();
-app.use(morgan('common'));
-app.use('/blog-posts', blogPostsRouter);
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+ljbkafjs
+asdf
+
+asdf
+
+asd sadfasd asdf
+
+muhahahahahahahahha
